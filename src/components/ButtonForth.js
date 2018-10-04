e9f0f79f4ba7ebe7f03401bde2d5f5ab368643be
Paolo Catalani <paolo@MBP-di-Paolo.localdomain>
2018-10-04 09:34:07 +0200

    First commit

--- src/components/ButtonForth.js
+++ src/components/ButtonForth.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowForwardIos from '@material-ui/icons/ArrowForwardIos';
 
 class ButtonForth extends React.Component {
   render() { 
     return ( 
       <div>
       <IconButton onClick={this.props.method} >
-        <DeleteIcon />
+        <ArrowForwardIos />
       </IconButton>
     </div>
      );
