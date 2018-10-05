0176ea1973bd1065e4e81a6cb41b534b95ea2c35
Paolo Catalani <paolo@MBP-di-Paolo.localdomain>
2018-10-05 16:08:09 +0200

    Add menu at NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import NavBar from './NavBar'
 // A modifier selon si notre logo sera svg ou dans le fichier logo.js
 // import logo from './logo.svg';
 
 import './Header.css';
-import MenuAppBar from './MenuAppBar';
  
   class Header extends Component {
     render() {
       return (
         <div>
-          <MenuAppBar />
+          <NavBar />
         </div>
       );
     }
