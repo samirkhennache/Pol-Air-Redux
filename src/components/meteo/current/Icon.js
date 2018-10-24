2b7f059e1279c5ac76923294bc9fdb6f6dd40c20
Matthieu <ranuccimatthieu@gmail.com>
2018-10-24 14:11:59 +0200

    modification des logo pour page d'accueil (avec dossier img dans dossier public

--- src/components/meteo/current/Icon.js
+++ src/components/meteo/current/Icon.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './Icon.css';
 
+
 class Icon extends React.Component{
 
   render() {
-    let icon = `http://openweathermap.org/img/w/${this.props.icon}.png`
-    
+    let icon = `/img/${this.props.icon}.png`
+
     return(
       <div>
         <img className="icon-meteo" src={icon} alt=""></img>
