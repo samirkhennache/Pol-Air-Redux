490c753ea02a73c135145f2ccc89ab3dd8ea0ae1
samirkhennache <samirkhennache@gmail.com>
2018-10-25 12:44:20 +0200

    amelioration du graphe

--- src/components/Pollution/HistoriquePollution.js
+++ src/components/Pollution/HistoriquePollution.js
@@ -1,15 +1,9 @@
 import React, {Component} from 'react';
 import {Line} from 'react-chartjs-2'
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
 import Hidden from '@material-ui/core/Hidden';
 import { Link } from 'react-router-dom'
 import './HistoriquePollution.css'
-
-
-
 
 
 ///declaration de varriables globales
@@ -18,9 +12,6 @@ let myDates = [];
 let myLabel = [];
 let myData  = [];
 let allData = null;
-
-
- 
 
 class HistoriquePollution extends Component {    
       
@@ -35,14 +26,19 @@ class HistoriquePollution extends Component {
         colorMonthHidden :'primary',
     }
     
-    //fonction qui lance le fecth asynchrone
+    /**
+     * fonction qui lance le fecth asynchrone
+     */
     GetChartData = async (e) => {
         const call = await fetch(url)
         allData = await call.json()
-        allData =allData.features.sort(function(a,b){return a.attributes.date_echea - b.attributes.date_echea}) 
-        //recuperation de data sur une année    
+        allData =allData.features.sort(function(a,b){return a.attributes.date_echea - b.attributes.date_echea})   
+        this.GetYear(); 
     }
 
+    /**
+     * recuperation data année
+     */
     GetYear(){
         //toute mes dates
         myLabel =[];
@@ -60,12 +56,15 @@ class HistoriquePollution extends Component {
 
 
         
-       const data = allData.map(element =>{
+        allData.map(element =>{
             let date  = new Date(element.attributes.date_echea)
             myLabel.push(date.toLocaleDateString('fr'));
             myData.push(element.attributes.valeur); 
             return element;
            })
+           /**
+            * filtre pour recuperer des data et date chaque 7 jours
+            */
            ///boucle pour filter les dates 
            let counter = 7;
            myLabel.map((element,index) =>{
@@ -83,18 +82,22 @@ class HistoriquePollution extends Component {
                }
                counter++;
                return element;
-           } )
+           })
+        /**
+         * push du dernier jour
+         */
         myDates.push(myLabel[myLabel.length-1]);
         myYearData.push(myData[myData.length-1])    
         greenZone.push(2);
         greenYellowZone.push(4);
         yellowZone.push(6);
         orangeZone.push(8);
-        redZone.push(10)
-        console.log(redZone);
-           
-           ///créer un clone de l'objet state 
-           chartData ={
+        redZone.push(10);
+        
+        /**
+         * créer un clone de l'objet state 
+         */
+        chartData ={
             labels:myDates,
             datasets:[{
                 label:'indice',
@@ -105,39 +108,46 @@ class HistoriquePollution extends Component {
                 data:myYearData,
             },
             {
+                label:'green',
                 pointRadius:0,
                 backgroundColor:'#79BC6A',
                 data:greenZone
             },
             {
+                label:'greenYellow',
                 pointRadius:0,
                 data:greenYellowZone,
                 backgroundColor:'#BBCF4C',
             },
             {
+                label:'yellow',
                 pointRadius:0,
                 data :yellowZone,
                 backgroundColor:'#EEC20B',
             },
             {
+                label:'orange',
                 pointRadius:0,
                 data:orangeZone,
                 backgroundColor:'#F29305',
             },
             {
+                label:'red',
                 pointRadius:0,
                 data:redZone,
                 backgroundColor:'#960018',
             },
         ]
         }
-        //mettre à jour les state
-           this.setState({chartData: chartData})
-           console.log(chartData);
-           
-          return(chartData)
+        /**
+         *mettre à jour les state
+         */
+        this.setState({chartData: chartData})
+          
         }
-        ///methode pour recuperer 1 mois 
+    /**
+     * methode pour recuperer 1 mois
+     */
     GetMonth(){
         let date =[];
         let data =[];
@@ -149,6 +159,9 @@ class HistoriquePollution extends Component {
         let orangeZone =[];
         let redZone =[];
         
+        /**
+         * filtre pour recuperer le dernier mois
+         */
         for (let i= 0; i<31; i++){
             date.push(myLabel[myLabel.length-31+i])
             data.push(myData[myData.length-31+i])
@@ -167,43 +180,49 @@ class HistoriquePollution extends Component {
                     label:'indice',
                     fill:false,
                     pointRadius:0,
-                    //backgroundColor:#BBCF4C,
                     borderColor:"#2C2A2A",
                     data:data,
                 },
-            {
-                pointRadius:0,
-                backgroundColor:'#79BC6A',
-                data:greenZone
-            },
-            {
-                pointRadius:0,
-                data:greenYellowZone,
-                backgroundColor:'#BBCF4C',
-            },
-            {
-                pointRadius:0,
-                data :yellowZone,
-                backgroundColor:'#EEC20B',
-            },
-            {
-                pointRadius:0,
-                data:orangeZone,
-                backgroundColor:'#F29305',
-            },
-            {
-                pointRadius:0,
-                data:redZone,
-                backgroundColor:'#960018',
-            },
+                {
+                    label:'green',
+                    pointRadius:0,
+                    backgroundColor:'#79BC6A',
+                    data:greenZone
+                },
+                {
+                    label:'greenYellow',
+                    pointRadius:0,
+                    data:greenYellowZone,
+                    backgroundColor:'#BBCF4C',
+                },
+                {
+                    label:'yellow',
+                    pointRadius:0,
+                    data :yellowZone,
+                    backgroundColor:'#EEC20B',
+                },
+                {
+                    label:'orange',
+                    pointRadius:0,
+                    data:orangeZone,
+                    backgroundColor:'#F29305',
+                },
+                {
+                    label:'red',
+                    pointRadius:0,
+                    data:redZone,
+                    backgroundColor:'#960018',
+                },
             ]
             }
        
         
-      
-       return(chartData)
-    }    
-    //methode pour recuperer les dates d'une semaine
+            //this.setState({chartData :chartData});
+            return(chartData);
+    }
+    /**
+     *methode pour recuperer les dates d'une semaine 
+     */
     GetWeek(){
         let date =[];
         let data =[];
@@ -213,6 +232,9 @@ class HistoriquePollution extends Component {
         let yellowZone =[];
         let orangeZone =[];
         let redZone =[];
+        /**
+         * filtre pour une semaine 
+         */
         for (let i=0 ;i<7; i++){
             date.push(myLabel[myLabel.length-7+i])
             data.push(myData[myData.length-7+i])
@@ -228,48 +250,54 @@ class HistoriquePollution extends Component {
                     label:'indice',
                     fill:false,
                     pointRadius:0,
-                    //backgroundColor:"#2C2A2A",
                     borderColor:"#2C2A2A",
                     data:data,
                 },
                 {
+                    label:'green',
                     pointRadius:0,
                     backgroundColor:'#79BC6A',
                     data:greenZone
                 },
                 {
+                    label:'greenYellow',
                     pointRadius:0,
                     data:greenYellowZone,
                     backgroundColor:'#BBCF4C',
                 },
                 {
+                    label:'yellow',
                     pointRadius:0,
                     data :yellowZone,
                     backgroundColor:'#EEC20B',
                 },
                 {
+                    label:'orange',
                     pointRadius:0,
                     data:orangeZone,
                     backgroundColor:'#F29305',
                 },
                 {
+                    label:'red',
                     pointRadius:0,
                     data:redZone,
                     backgroundColor:'#960018',
                 },
             ]
-            }   
-       return(chartData)
+            }  
+            //this.setState({chartData :chartData})
+            return(chartData);
     }
 
     componentDidMount() {
-       ///recuperation des données au lancement du DOM
         this.GetChartData(); 
         
     }
-    ///evement qui lorsque on passe d'un onglet à un autre 
+    /**
+     * evements qui lorsque on passe d'un onglet à un autre 
+     */
     handleChangeYear = () => {
-        this.GetYear()
+        
         this.setState({ value :0,
                         onlyOne :true,
                         colorYear :'primary',
@@ -301,15 +329,12 @@ class HistoriquePollution extends Component {
                     });
         
       };
-    render() {
-        const { classes } = this.props;
-       
-        
-        console.log(classes);
-        
+    render() { 
         return (
         <div className = 'blocGraph'>
-           <div className ='btns-graph'>
+           
+            <div className ='graph'>
+            <div className ='btns-graph'>
             <Hidden smDown>
                 <Button 
                 variant="contained" 
@@ -319,7 +344,6 @@ class HistoriquePollution extends Component {
                 >
                 Année
                 </Button>
-                
                 <Button 
                 variant="contained" 
                 color={this.state.colorMonth}
@@ -357,11 +381,10 @@ class HistoriquePollution extends Component {
                 </Button>
             </Hidden>
             </div> 
-            <div className ='graph'>
             <Hidden smDown>
-            {this.state.value === 0 && <Line data ={this.state.chartData}
+            {this.state.value === 0 && <Line key='year-full' data ={this.state.chartData}
                  options={{
-                     
+                    responsive:true,
                     legend:{
                         display:false
                     },
@@ -380,7 +403,7 @@ class HistoriquePollution extends Component {
                        }          
                 }}
             />}
-            {this.state.value === 1 && <Line data ={this.state.chartData} 
+            {this.state.value === 1 && <Line key ='month-full' data ={this.GetMonth()} 
              options={{
                  legend:{
                      display:false
@@ -389,14 +412,6 @@ class HistoriquePollution extends Component {
                     display: false,
                     text: 'Indice de Pollution',
                     fontSize:30,
-                },
-                layout: {
-                    padding: {
-                        left: 0,
-                        right: 0,
-                        top: 0,
-                        bottom: 0
-                    }
                 },
                     scales:{
                         yAxes:[{
@@ -408,8 +423,7 @@ class HistoriquePollution extends Component {
                     }          
                 }}
             />}
-            {/* declenche la recuperation des données pour la semaine*/}
-            {!this.onlyOne&&this.state.value === 2 &&  <Line data ={this.state.chartData} 
+            {!this.onlyOne&&this.state.value === 2 &&  <Line key ='week-full' data ={this.GetWeek()} 
              options={{
                 legend:{
                     display:false
@@ -430,9 +444,8 @@ class HistoriquePollution extends Component {
                 }}
             />}
             </Hidden>
-            {/* declenche la recuperation des données pour le mois  */}
             <Hidden mdUp>
-            {(this.state.value === 1||this.state.onlyOne) && <Line data ={this.state.chartData} 
+            {(this.state.value === 1||this.state.onlyOne) && <Line key= 'month' data ={this.GetMonth()} 
              options={{
                 legend:{
                     display:false
@@ -442,12 +455,7 @@ class HistoriquePollution extends Component {
                     text: 'Indice de Pollution',
                     fontSize:30,
                 },
-                padding: {
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0
-                },
+                 
                 scales:{
                         yAxes:[{
                             ticks :{
@@ -458,8 +466,7 @@ class HistoriquePollution extends Component {
                     }          
                 }}
             />}
-            {/* declenche la recuperation des données pour la semaine*/}
-            {  this.state.value === 2 &&  <Line data ={this.state.chartData} 
+            {  this.state.value === 2 &&  <Line key='week' data ={this.GetWeek()} 
              options={{
                 legend:{
                     display:false
@@ -468,12 +475,6 @@ class HistoriquePollution extends Component {
                     display: false,
                     text: 'Indice de Pollution',
                     fontSize:30,
-                },
-                padding: {
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0
                 },
                 scales:{
                         yAxes:[{
