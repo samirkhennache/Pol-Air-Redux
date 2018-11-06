870342ca862c6c5a29df3bef60112a64cc166245
Prudence Gaboriau <prudence.gaboriau@gmail.com>
2018-11-06 17:03:13 +0100

    modifs css page pol

--- src/components/Pollution/PagePollution.js
+++ src/components/Pollution/PagePollution.js
@@ -18,10 +18,10 @@ class PagePollution extends Component {
                 {this.props.loading ? "En cours de chargement" :
                     <PagePollutionToday city={this.props.city} indice={this.props.indice} imgBackground={this.props.imgBackground} />
                 }
-                <div className="bloc-conseilHistorique">
-                <ConseilsPollution className="test-border" indice={this.props.indice}/>
-                <BlocPolluants/>
-                <HistoriquePollution/>
+                <div className="bloc-conseil-historique">
+                    <ConseilsPollution className="test-border" indice={this.props.indice}/>
+                    <BlocPolluants/>
+                    <HistoriquePollution/>
                 </div>
             </div>
         );
