2ff3549f07e9d70a6da0ba5d16cb6f161374f7e0
samirkhennache <samirkhennache@gmail.com>
2018-12-17 13:45:23 +0100

    bug fixed

--- src/components/Pollution/SmileyPollution.js
+++ src/components/Pollution/SmileyPollution.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './smileyPollution.css'
 import Smiley from './Smiley'
 
-const GetSmiley = ({indice}) => {
+const GetSmiley = (indice) => {
   if(indice<=25)
     return 'smiley-tres-faible'
   else if(indice<=50)
@@ -15,13 +15,12 @@ const GetSmiley = ({indice}) => {
     return'smiley-tres-eleve'
   }
 
-const SmileyPollution = ({indice}) => (
+const SmileyPollution = ({indice}) =>(
   <div className="smiley-main">
     <div className={`smiley-solo`}>
       <Smiley img={GetSmiley(indice)} />
     </div>
   </div>
 );
-
 
 export default SmileyPollution;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import store from './store'
 import  {Provider} from 'react-redux'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker';
+import * as ServiceWorker from './serviceWorker';
+// import registerServiveWorker from './registerServiveWorker'
 
 ReactDOM.render(<Provider store ={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-registerServiceWorker();
+ServiceWorker.register();
